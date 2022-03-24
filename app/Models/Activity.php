8a96f7b53e68bff6e06acaf32091f5c259846357
Steven Alexander <alexgrant200@yahoo.com>
2022-03-24 09:36:16 +0700

    fix: adding

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -7,32 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Activity extends Model
 {
-  use HasFactory;
+	use HasFactory;
 
-  protected $with = ['driver', 'vehicle', 'project', 'departureLocation', 'arrivalLocation'];
+	protected $guarded = ['id'];
 
-  public function driver()
-  {
-    return $this->belongsTo(User::class, 'user_id');
-  }
+	public function driver()
+	{
+		return $this->belongsTo(User::class, 'user_id');
+	}
 
-  public function vehicle()
-  {
-    return $this->belongsTo(Vehicle::class);
-  }
+	public function vehicle()
+	{
+		return $this->belongsTo(Vehicle::class);
+	}
 
-  public function project()
-  {
-    return $this->belongsTo(Project::class);
-  }
+	public function project()
+	{
+		return $this->belongsTo(Project::class);
+	}
 
-  public function departureLocation()
-  {
-    return $this->belongsTo(Address::class, 'departure_location_id');
-  }
+	public function departureLocation()
+	{
+		return $this->belongsTo(Address::class, 'departure_location_id');
+	}
 
-  public function arrivalLocation()
-  {
-    return $this->belongsTo(Address::class, 'arrival_location_id');
-  }
+	public function arrivalLocation()
+	{
+		return $this->belongsTo(Address::class, 'arrival_location_id');
+	}
 }
