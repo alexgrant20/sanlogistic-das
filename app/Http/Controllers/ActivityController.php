1574415fde01d707419d0cf3eb528092dced5d16
Steven Alexander <alexgrant200@yahoo.com>
2022-03-24 09:35:46 +0700

    feat: create activity logic

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Activity;
 use App\Http\Requests\StoreActivityRequest;
 use App\Http\Requests\UpdateActivityRequest;
+use App\Models\Address;
+use App\Models\Person;
+use App\Models\Project;
+use App\Models\User;
+use App\Models\Vehicle;
+use Exception;
+use Illuminate\Support\Facades\DB;
 
 class ActivityController extends Controller
 {
@@ -15,7 +22,9 @@ class ActivityController extends Controller
 	 */
 	public function index()
 	{
-		return view('activities.index');
+		return view('activities.index', [
+			'title' => 'Activities',
+		]);
 	}
 
 	/**
@@ -25,7 +34,13 @@ class ActivityController extends Controller
 	 */
 	public function create()
 	{
-		return view('activities.create');
+		return view('activities.create', [
+			'title' => 'Create Activity',
+			'addresses' => Address::orderBy('name', 'ASC')->get(),
+			'vehicles' => Vehicle::orderBy('license_plate', 'ASC')->get(),
+			'projects' => Project::orderBy('name', 'ASC')->get(),
+			'users' => User::with('person')->get(),
+		]);
 	}
 
 	/**
@@ -36,7 +51,39 @@ class ActivityController extends Controller
 	 */
 	public function store(StoreActivityRequest $request)
 	{
-		//
+		// $money = preg_replace("/[^0-9]/", "", $request->safe()->only('bbm_amount'));
+		// var_dump((int) $money['bbm_amount']);
+
+		try {
+			$moneyTable = ['bbm_amount', 'parking', 'retribution_amount', 'parking', 'toll_amount'];
+			$imageTable = ['do', 'departure_odo', 'arrival_odo', 'bbm', 'toll', 'retribution', 'parking'];
+
+			$data = $request->safe()->all();
+			$person_name = User::with('person')->where('id', '=', $data['user_id'])->get()[0]->person->name;
+			$timestamp = now()->timestamp;
+
+			foreach ($moneyTable as $x) $data[$x] = preg_replace("/[^0-9]/", "", $data[$x]);
+
+			foreach ($imageTable as $x) {
+				$imagePath = "";
+				if ($request->file("{$x}_image")) {
+					$fileName = "{$x}-{$person_name}-{$timestamp}.{$request->file("{$x}_image")->extension()}";
+					$imagePath = $request->file("{$x}_image")->storeAs("{$x}-images", $fileName, 'public');
+				}
+				$data["{$x}_image"] = $imagePath;
+			}
+
+			DB::beginTransaction();
+
+			Activity::create($data);
+
+			DB::commit();
+
+			return redirect('/activities')->with('success', 'New company has been added!');
+		} catch (Exception $e) {
+			DB::rollback();
+			return redirect('/activities/create')->withInput()->with('error', $e->getMessage());
+		}
 	}
 
 	/**
@@ -82,5 +129,6 @@ class ActivityController extends Controller
 	public function destroy(Activity $activity)
 	{
 		//
+
 	}
 }
