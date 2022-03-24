515e098e08fcf61d72507a8b8c185b6bc040e3f0
Steven Alexander <alexgrant200@yahoo.com>
2022-03-24 11:56:18 +0700

    fix: change user relationship to hasOne

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -7,37 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-  use HasFactory;
+	use HasFactory;
 
-  protected $guarded = ['id'];
+	protected $guarded = ['id'];
 
-  public function project()
-  {
-    return $this->belongsTo(Project::class);
-  }
+	public function project()
+	{
+		return $this->belongsTo(Project::class);
+	}
 
-  public function department()
-  {
-    return $this->belongsTo(Department::class);
-  }
+	public function department()
+	{
+		return $this->belongsTo(Department::class);
+	}
 
-  public function area()
-  {
-    return $this->belongsTo(Area::class);
-  }
+	public function area()
+	{
+		return $this->belongsTo(Area::class);
+	}
 
-  public function address()
-  {
-    return $this->belongsTo(Address::class);
-  }
+	public function address()
+	{
+		return $this->belongsTo(Address::class);
+	}
 
-  public function user()
-  {
-    return $this->belongsTo(User::class);
-  }
+	public function user()
+	{
+		return $this->hasOne(User::class);
+	}
 
-  public function personDocuments()
-  {
-    return $this->hasMany(PersonDocument::class);
-  }
+	public function personDocuments()
+	{
+		return $this->hasMany(PersonDocument::class);
+	}
 }
