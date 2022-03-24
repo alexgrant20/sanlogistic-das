69472f9309fb7044d4daf222244b63ab65675a6f
Steven Alexander <alexgrant200@yahoo.com>
2022-03-24 11:52:12 +0700

    feat: updating activity

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -24,6 +24,7 @@ class ActivityController extends Controller
 	{
 		return view('activities.index', [
 			'title' => 'Activities',
+			'activities' => Activity::latest()->with('driver', 'vehicle', 'departureLocation', 'arrivalLocation')->get()
 		]);
 	}
 
@@ -51,9 +52,6 @@ class ActivityController extends Controller
 	 */
 	public function store(StoreActivityRequest $request)
 	{
-		// $money = preg_replace("/[^0-9]/", "", $request->safe()->only('bbm_amount'));
-		// var_dump((int) $money['bbm_amount']);
-
 		try {
 			$moneyTable = ['bbm_amount', 'parking', 'retribution_amount', 'parking', 'toll_amount'];
 			$imageTable = ['do', 'departure_odo', 'arrival_odo', 'bbm', 'toll', 'retribution', 'parking'];
@@ -105,7 +103,14 @@ class ActivityController extends Controller
 	 */
 	public function edit(Activity $activity)
 	{
-		return view('activities.edit');
+		return view('activities.edit', [
+			'title' => 'Update Activity',
+			'activity' => $activity,
+			'addresses' => Address::orderBy('name', 'ASC')->get(),
+			'vehicles' => Vehicle::orderBy('license_plate', 'ASC')->get(),
+			'projects' => Project::orderBy('name', 'ASC')->get(),
+			'users' => User::with('person')->get(),
+		]);
 	}
 
 	/**
@@ -117,7 +122,36 @@ class ActivityController extends Controller
 	 */
 	public function update(UpdateActivityRequest $request, Activity $activity)
 	{
-		//
+		try {
+			$moneyTable = ['bbm_amount', 'parking', 'retribution_amount', 'parking', 'toll_amount'];
+			$imageTable = ['do', 'departure_odo', 'arrival_odo', 'bbm', 'toll', 'retribution', 'parking'];
+
+			$data = $request->safe()->all();
+			$person_name = User::with('person')->where('id', '=', $data['user_id'])->get()[0]->person->name;
+			$timestamp = now()->timestamp;
+
+			foreach ($moneyTable as $x) $data[$x] = preg_replace("/[^0-9]/", "", $data[$x]);
+
+			foreach ($imageTable as $x) {
+				$imagePath = $activity["{$x}_image"];
+				if ($request->file("{$x}_image")) {
+					$fileName = "{$x}-{$person_name}-{$timestamp}.{$request->file("{$x}_image")->extension()}";
+					$imagePath = $request->file("{$x}_image")->storeAs("{$x}-images", $fileName, 'public');
+				}
+				$data["{$x}_image"] = $imagePath;
+			}
+
+			DB::beginTransaction();
+
+			$activity->update($data);
+
+			DB::commit();
+
+			return redirect('/activities')->with('success', 'Company has been updated!');
+		} catch (Exception $e) {
+			DB::rollback();
+			return redirect("/activities/{$activity->id}/edit")->withInput()->with('error', $e->getMessage());
+		}
 	}
 
 	/**
