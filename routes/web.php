05493e17f9dc4dc4a4bc3732159c3c788f115aa6
STEVEN ALEXANDER GRANT <steven.grant@binus.ac.id>
2022-03-25 13:54:00 +0700

    feat: adding middleware

--- routes/web.php
+++ routes/web.php
@@ -33,23 +33,23 @@ Route::post('/login', [LoginController::class, 'authenticate'])->middleware('gue
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 
 // Route::get('/addresses/getAllLocation')
-Route::get('/addresses/export/excel', [AddressController::class, 'exportExcel']);
+Route::get('/addresses/export/excel', [AddressController::class, 'exportExcel'])->middleware('auth');
 Route::resource('/addresses', AddressController::class)->middleware('auth');
 
-Route::get('/activities/export/excel', [ActivityController::class, 'exportExcel']);
+Route::get('/activities/export/excel', [ActivityController::class, 'exportExcel'])->middleware('auth');
 Route::resource('/activities', ActivityController::class)->middleware('auth');
 
-Route::get('/people/export/excel', [PersonController::class, 'exportExcel']);
+Route::get('/people/export/excel', [PersonController::class, 'exportExcel'])->middleware('auth');
 Route::resource('/people', PersonController::class)->middleware('auth');
 
-Route::get('/vehicles/export/excel', [VehicleController::class, 'exportExcel']);
-Route::post('/vehicles/import/excel', [VehicleController::class, 'importExcel']);
+Route::get('/vehicles/export/excel', [VehicleController::class, 'exportExcel'])->middleware('auth');
+Route::post('/vehicles/import/excel', [VehicleController::class, 'importExcel'])->middleware('auth');
 Route::get('/vehicles/migrate/image', [VehicleController::class, 'migrateImage'])->middleware('auth');
 Route::resource('/vehicles', VehicleController::class)->middleware('auth');
 
-Route::get('/companies/export/excel', [CompanyController::class, 'exportExcel']);
+Route::get('/companies/export/excel', [CompanyController::class, 'exportExcel'])->middleware('auth');
 Route::get('/companies/migrate/image', [CompanyController::class, 'migrateImage'])->middleware('auth');
 Route::resource('/companies', CompanyController::class)->middleware('auth');
 
-Route::get('/projects/export/excel', [ProjectController::class, 'exportExcel']);
+Route::get('/projects/export/excel', [ProjectController::class, 'exportExcel'])->middleware('auth');
 Route::resource('/projects', ProjectController::class)->middleware('auth');
