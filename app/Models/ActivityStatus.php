394c6076a8a8f5e761367861770b9a9eed3041ff
STEVEN ALEXANDER GRANT <steven.grant@binus.ac.id>
2022-03-30 06:49:25 +0700

    fix: adding guarded var

--- app/Models/ActivityStatus.php
+++ app/Models/ActivityStatus.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Blameable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ActivityStatus extends Model
 {
-  use HasFactory;
+  use HasFactory, Blameable;
+
+  protected $guarded = ['id'];
 
   public function activity()
   {
