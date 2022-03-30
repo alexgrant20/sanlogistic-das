af6354adb6a9968bdc05ad455d2873ddf17f3e73
Steven Alexander <alexgrant200@yahoo.com>
2022-03-30 13:17:26 +0700

    feat: method to approve activity

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Activity;
+use App\Models\ActivityStatus;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FinanceController extends Controller
 {
   public function acceptance()
   {
     return view('finance.acceptance', [
-      'activities' => Activity::whereRelation('activityStatus', 'status', '=', 'pending')->get(),
+      'activities' => Activity::status('pending')->get(),
       'importPath' => '/finance/acceptance/import/excel',
       'title' => 'Acceptance'
     ]);
@@ -19,9 +22,32 @@ class FinanceController extends Controller
   public function payment()
   {
     return view('finance.payment', [
-      'activities' => Activity::whereRelation('activityStatus', 'status', '=', 'approved')->get(),
+      'activities' => Activity::status('approved')->get(),
       'importPath' => '/finance/acceptance/import/excel',
       'title' => 'Acceptance'
     ]);
+  }
+
+  public function approve(Request $request)
+  {
+    try {
+      $ids = json_decode($request->getContent());
+
+      DB::beginTransaction();
+
+      foreach ($ids as $id) {
+        ActivityStatus::create([
+          'activity_id' => $id,
+          'status' => 'approved',
+        ]);
+      }
+
+      DB::commit();
+
+      session()->flash('success', 'Activity approved!');
+    } catch (Exception $e) {
+      DB::rollBack();
+      session()->flash('error', 'Failed to approved!');
+    }
   }
 }
