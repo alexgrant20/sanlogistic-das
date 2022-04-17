d7121ed97cd686e2ae9f54880714384bf75bb298
Steven Alexander <alexgrant200@yahoo.com>
2022-04-17 23:27:39 +0700

    feat: auto policies register

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Address;
+use App\Models\User;
 use App\Policies\AddressPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -12,14 +13,12 @@ class AuthServiceProvider extends ServiceProvider
   /**
    * The policy mappings for the application.
    *
-   * @var array<class-string, class-string>
+   * @var array
    */
-  protected $policies = [
-    Address::class => AddressPolicy::class
-  ];
+  protected $policies = [];
 
   /**
-   * Register any authentication / authorization services.
+   * Register any application authentication / authorization services.
    *
    * @return void
    */
