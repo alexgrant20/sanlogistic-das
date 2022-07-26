f45ae2e2198af735dba828e58efb18fcc8d63f9a
STEVEN ALEXANDER GRANT <steven.grant@binus.ac.id>
2022-07-26 17:30:28 +0700

    bug: changing viewport

--- resources/views/driver/layouts/main.blade.php
+++ resources/views/driver/layouts/main.blade.php
@@ -6,8 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Sanlogistic | {{ $title }}</title>
   <meta name="description" content="">
-  <meta name="viewport"
-    content="width=device-width, initial-scale=0.8, shrink-to-fit=no,maximum-scale=0.8, user-scalable=0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no,maximum-scale=1, user-scalable=0">
   <meta name="robots" content="all,follow">
 
   @livewireStyles
