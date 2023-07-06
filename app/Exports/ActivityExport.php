a43493ad85b02aab41cea4a8a266df5dc0689404
Steven Alexander <alexgrant200@yahoo.com>
2023-07-06 11:53:50 +0700

    Fix wrong column taken in activity export

--- app/Exports/ActivityExport.php
+++ app/Exports/ActivityExport.php
@@ -65,7 +65,7 @@ class ActivityExport implements FromCollection, WithHeadings, ShouldAutoSize
         '' AS IFRETRIBUTION_EXPENSES,
         '' AS ZONE,
         '' AS DCODE,
-        arrival_odo - departure_odo AS DISTANCE,
+        CONCAT(arrival_odo - departure_odo, ' Km') AS DISTANCE,
         activity_payments.bbm_amount + activity_payments.toll_amount
    + activity_payments.parking_amount + activity_payments.load_amount + activity_payments.unload_amount + activity_payments.maintenance_amount AS total_cost,
         projects.name AS projects_name,
@@ -75,7 +75,7 @@ class ActivityExport implements FromCollection, WithHeadings, ShouldAutoSize
         CONCAT(ROUND((arrival_odo - departure_odo) / (TIMESTAMPDIFF(SECOND, departure_date, arrival_date) / 3600)), ' Km/Jam') AS average_speed,
         activities.id,
         activities.parent_activity_id,
-        arrival_odo - departure_odo,
+        CONCAT(ai.total_distance, ' Km'),
         CASE WHEN ai.is_half_trip = 1 THEN 'half'
         WHEN ai.is_half_trip = 0 THEN 'full'
         END,
