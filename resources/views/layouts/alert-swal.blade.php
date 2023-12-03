c88c6f315368584ba2513853fba4370123fc0320
Grant <alexgrant200@yahoo.com>
2023-12-03 22:09:16 +0700

    fix swal usage

--- resources/views/layouts/alert-swal.blade.php
+++ resources/views/layouts/alert-swal.blade.php
@@ -1,37 +1,37 @@
 <script>
-	function generateSwalPayload(title, message, icon) {
-		 let form = document.createElement("div");
-		 form.innerHTML = message;
-		 return {
-				icon,
-				title,
-				html: true,
-				content: form,
-				confirmButtonText: "Tutup"
-		 }
-	}
+  function generateSwalPayload(title, message, icon) {
+    let form = document.createElement("div");
+    form.innerHTML = message;
+    return {
+      icon,
+      title,
+      html: true,
+      content: form,
+      confirmButtonText: "Tutup"
+    }
+  }
 </script>
 
 @if ($message = Session::get('success-swal'))
-	<script>
-		 swal(generateSwalPayload("Success", `{!! $message !!}`, "success"));
-	</script>
+  <script>
+    Swal.fire(generateSwalPayload("Success", `{!! $message !!}`, "success"));
+  </script>
 @endif
 
 @if ($message = Session::get('error-swal'))
-	<script>
-		 swal(generateSwalPayload("Failed", `{!! $message !!}`, "error"));
-	</script>
+  <script>
+    Swal.fire("Failed", `{!! $message !!}`, "error");
+  </script>
 @endif
 
 @if ($message = Session::get('warning-swal'))
-	<script>
-		 swal(generateSwalPayload("Warning", `{!! $message !!}`, "warning"));
-	</script>
+  <script>
+    Swal.fire(generateSwalPayload("Warning", `{!! $message !!}`, "warning"));
+  </script>
 @endif
 
 @if ($message = Session::get('info-swal-html'))
-	<script>
-		 swal(generateSwalPayload("Info", `{!! $message !!}`, "info"));
-	</script>
+  <script>
+    Swal.fire(generateSwalPayload("Info", `{!! $message !!}`, "info"));
+  </script>
 @endif
